053d245609038e187b854f662af68be99f6671da
rajcodes168h <raj19all@gmail.com>
2024-04-20 17:02:35 +0530

    swagger docs

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,41 @@
 const express = require('express');
-
 require("dotenv").config();
+
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUI = require("swagger-ui-express")
 
 const app = express();
 const {connection} = require("./config/db");
+const {userRouter} = require('./routes/user.routes');
+
+app.use("/",userRouter);
 
 app.get('/',(req,res)=>{
     res.send({msg : "hello world"});
 })
 
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'User Management World',
+        version: '1.3.0',
+      },
+      servers:[
+        {
+          url:"http://localhost:8080/"
+        },
+        {
+          url:"http://wwww.example.com"
+        }
+      ]
+    },
+    apis: ['./routes/*.js'], 
+};
+const openapiSpecification = swaggerJsdoc(options);
+
+app.use('/apidocs', swaggerUI.serve, swaggerUI.setup(openapiSpecification));
 
 app.listen(process.env.port, async()=>{
     try {
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -9,6 +9,89 @@ const {UserModel} = require("../model/user.model");
 const { RestaurantModel } = require("../model/restaurant.model");
 const { OrderModel } = require("../model/order.model");
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         name:
+ *           type: string
+ *           description:  the user name
+ *         email:
+ *           type: string
+ *           description: the user email
+ *         password:
+ *           type: string
+ *           description: the user email
+ *         address: 
+ *           type: object
+ *           properties:
+ *               street:
+ *                    type : string
+ *                    description: The street of the user
+ *               city:
+ *                    type : string
+ *                    description: The city of the user 
+ *               state:
+ *                    type : string
+ *                    description: The state of the user
+ *               country:
+ *                    type : string
+ *                    description: The country of the user
+ *               zip:
+ *                    type : string
+ *                    description: The zip of the user
+ */
+
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *      summary: This will get all the users data from database
+ *      tags: [Users]
+ *      responses:
+ *          200:
+ *              description: The list of all the users
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          item:
+ *                              $ref:"#/components/schemas/User"
+ *          400:
+ *               description: some error occured
+ */
+
+
+
+/**
+ * @swagger
+ * /users/api/register:
+ *  post:
+ *      summary: This post the new user details data from database
+ *      tags: [Users]
+ *      requestBody: 
+ *          required: true
+ *          content:
+ *              application/json:
+ *              schema:
+ *                  $ref: "#/components/schemas/User"
+ *      responses:
+ *          200:
+ *              description: To add a new user to list of all the users
+ *              content:
+ *                  application/json:
+ *                      schema:                
+ *                          item:
+ *                              $ref:"#/components/schemas/User"
+ *          400: 
+ *               description: internal server error
+ */
 
 userRouter.post('/api/register', async(req,res)=>{
     const {username, email, password} = req.body;
@@ -31,6 +114,36 @@ userRouter.post('/api/register', async(req,res)=>{
         console.log("error", error);
     }
 })
+
+
+/**
+* @swagger
+* /users/{id}:
+*  patch:
+*       summary: Update a user by ID
+*       description: Update an existing user identified by their ID.
+*       parameters:
+*         - name: id
+*           in: path
+*           description: ID of the user to update
+*           required: true
+*           schema:
+*             type: string
+*         - name: body
+*           in: body
+*           description: Data to update the user with
+*           required: true
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/User'
+*       responses:
+*         200:
+*           description: Successfully updated user
+*         404:
+*           description: User not found
+*/
+
 
 userRouter.post('/api/login',async(req,res)=>{
     const {email, password} = req.body;
@@ -98,6 +211,54 @@ userRouter.post('/api/restaurants', async(req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/restaurants:
+ *   post:
+ *     summary: Add a new restaurant
+ *     tags: [Restaurants]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               address:
+ *                 type: object
+ *                 properties:
+ *                   street:
+ *                     type: string
+ *                   city:
+ *                     type: string
+ *                   state:
+ *                     type: string
+ *                   country:
+ *                     type: string
+ *                   zip:
+ *                     type: string
+ *               menu:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                     description:
+ *                       type: string
+ *                     price:
+ *                       type: number
+ *                     image:
+ *                       type: string
+ *     responses:
+ *       '201':
+ *         description: Restaurant created successfully
+ *       '401':
+ *         description: creation failed
+ */      
+
 userRouter.get('/api/restaurants', async(req, res) => {
     try {
         const restaurants = await RestaurantModel.find();
@@ -108,6 +269,52 @@ userRouter.get('/api/restaurants', async(req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/restaurants/:id
+ *   get:
+ *     summary: get restaurant models
+ *     tags: [Restaurants]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               address:
+ *                 type: object
+ *                 properties:
+ *                   street:
+ *                     type: string
+ *                   city:
+ *                     type: string
+ *                   state:
+ *                     type: string
+ *                   country:
+ *                     type: string
+ *                   zip:
+ *                     type: string
+ *               menu:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                     description:
+ *                       type: string
+ *                     price:
+ *                       type: number
+ *                     image:
+ *                       type: string
+ *     responses:
+ *       '201':
+ *         description: Restaurant retrived successfully
+ *     
+ */
 userRouter.get('/api/restaurants/:id', async(req, res) => {
     const { id } = req.params;
 
@@ -121,7 +328,74 @@ userRouter.get('/api/restaurants/:id', async(req, res) => {
         console.error(error);
         return res.status(500).json({ msg: 'Internal server error' });
     }
-});
+}); 
+
+/**
+ * @swagger
+ * /api/restaurants/{id}/menu:
+ *   get:
+ *     summary: Get the menu of a specific restaurant
+ *     tags: [Restaurants]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the restaurant
+ *     responses:
+ *       '200':
+ *         description: Menu retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ *                   description:
+ *                     type: string
+ *                   price:
+ *                     type: number
+ *                   image:
+ *                     type: string
+ *   put:
+ *     summary: Add a new item to a restaurant's menu
+ *     tags: [Restaurants]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the restaurant
+ *       - in: body
+ *         name: menuItem
+ *         required: true
+ *         description: The menu item to add
+ *         schema:
+ *           type: object
+ *           properties:
+ *             name:
+ *               type: string
+ *             description:
+ *               type: string
+ *             price:
+ *               type: number
+ *             image:
+ *               type: string
+ *     responses:
+ *       '201':
+ *         description: Menu item added successfully
+ *       '404':
+ *         description: Restaurant not found
+ *       '500':
+ *         description: Internal server error
+ */
 
 userRouter.get('/api/restaurants/:id/menu', async(req, res) => {
     const { id } = req.params;
@@ -159,6 +433,34 @@ userRouter.put('/api/restaurants/:id/menu', async(req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/restaurants/{id}/menu/{menuId}:
+ *   delete:
+ *     summary: Delete a menu item from a restaurant's menu
+ *     tags: [Restaurants]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the restaurant
+ *       - in: path
+ *         name: menuId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the menu item
+ *     responses:
+ *       '202':
+ *         description: Menu item deleted successfully
+ *       '404':
+ *         description: Restaurant not found
+ *       '500':
+ *         description: Internal server error
+ */
+
 userRouter.delete('/api/restaurants/:id/menu/:menuId', async(req, res) => {
     const { id, menuId } = req.params;
 
@@ -178,6 +480,58 @@ userRouter.delete('/api/restaurants/:id/menu/:menuId', async(req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/orders:
+ *   post:
+ *     summary: Place a new order
+ *     tags: [Orders]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user:
+ *                 type: string
+ *               restaurant:
+ *                 type: string
+ *               items:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                     price:
+ *                       type: number
+ *                     quantity:
+ *                       type: number
+ *               totalPrice:
+ *                 type: number
+ *               deliveryAddress:
+ *                 type: object
+ *                 properties:
+ *                   street:
+ *                     type: string
+ *                   city:
+ *                     type: string
+ *                   state:
+ *                     type: string
+ *                   country:
+ *                     type: string
+ *                   zip:
+ *                     type: string
+ *               status:
+ *                 type: string
+ *     responses:
+ *       '201':
+ *         description: Order placed successfully
+ *       '500':
+ *         description: Internal server error
+ */
+
 userRouter.post('/api/orders', async(req, res) => {
     const { user, restaurant, items, totalPrice, deliveryAddress, status } = req.body;
 
@@ -190,6 +544,101 @@ userRouter.post('/api/orders', async(req, res) => {
         return res.status(500).json({ msg: 'Internal server error' });
     }
 });
+
+/**
+ * @swagger
+ * /api/orders/{id}:
+ *   get:
+ *     summary: Get details of a specific order
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the order
+ *     responses:
+ *       '200':
+ *         description: Order details retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                     name:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *                 restaurant:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                     name:
+ *                       type: string
+ *                 items:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       name:
+ *                         type: string
+ *                       price:
+ *                         type: number
+ *                       quantity:
+ *                         type: number
+ *                 totalPrice:
+ *                   type: number
+ *                 deliveryAddress:
+ *                   type: object
+ *                   properties:
+ *                     street:
+ *                       type: string
+ *                     city:
+ *                       type: string
+ *                     state:
+ *                       type: string
+ *                     country:
+ *                       type: string
+ *                     zip:
+ *                       type: string
+ *                 status:
+ *                   type: string
+ *   put:
+ *     summary: Update the status of a specific order
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the order
+ *       - in: body
+ *         name: status
+ *         required: true
+ *         description: The new status of the order
+ *         schema:
+ *           type: object
+ *           properties:
+ *             status:
+ *               type: string
+ *     responses:
+ *       '204':
+ *         description: Order status updated successfully
+ *       '404':
+ *         description: Order not found
+ *       '500':
+ *         description: Internal server error
+ */
 
 userRouter.get('/api/orders/:id', async(req, res) => {
     const { id } = req.params;
